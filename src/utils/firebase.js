5a80ffc8fc348f8a922811ea9e105426eb74c2a2
joeyj364 <joeyjnrgold@gmail.com>
2021-02-19 14:46:50 +0000

    Refactor: Put firebase config in process.env

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/auth';
 
 // * Firebase App Config
 const firebaseConfig = {
-  apiKey: 'AIzaSyDlUfvK2q51wfpn7bIL2TF14rf5ZxDbRfQ',
-  authDomain: 'netflix-clone-4caa6.firebaseapp.com',
-  databaseURL: 'https://netflix-clone-4caa6.firebaseio.com',
-  projectId: 'netflix-clone-4caa6',
-  storageBucket: 'netflix-clone-4caa6.appspot.com',
-  messagingSenderId: '154053434488',
-  appId: '1:154053434488:web:2026c491662d83dfb0e18d',
-  measurementId: 'G-1ZNH5PL90T',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
